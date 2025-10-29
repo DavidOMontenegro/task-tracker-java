6abbbc9cd482a2935071f6b3e35018ba47d550b0
DavidOMontenegro <davidom.official@gmail.com>
2025-10-29 17:48:05 -0300

    Add sample hello, world code

---
+++ src/main/java/com/davidomontenegro/tasktracker/TaskTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.davidomontenegro.tasktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class TaskTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerApplication.class, args);
+    }
+
+}
